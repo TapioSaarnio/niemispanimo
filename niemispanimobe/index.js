3cf67c28deb623082e1502a2ceb46cabcb21045e
TapioSaarnio <tapio.saarnio@gmail.com>
2021-05-05 20:08:47 +0300

    routing

--- niemispanimobe/index.js
+++ niemispanimobe/index.js
@@ -3,7 +3,7 @@ const http = require('http')
 const config = require('./utils/config')
 
 const server = http.createServer(app)
-app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')))
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../build', 'index.html')))
 
 server.listen(config.PORT, () => {
   console.log(`Server running from port ${process.env.PORT}`)
