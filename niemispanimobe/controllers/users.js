45c812297de4f174be88084b0824f989f1d95b8a
TapioSaarnio <tapio.saarnio@gmail.com>
2021-05-05 13:59:01 +0300

    turhia poistettu

--- niemispanimobe/controllers/users.js
+++ niemispanimobe/controllers/users.js
@@ -2,29 +2,8 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
-/*
-Returns all users
-*/
-usersRouter.get('/', (request, response) => {
-
-    User.find({}).then(u => {
-        response.json(u.map(u => u.toJSON()))
-    })
-})
-
-/*
-Returns a user with a certain ID
-*/
-usersRouter.get('/:id', (request, response, next) => {
 
 
-    User.findById(request.params.id).then(u => {
-        if(u) {
-        response.json(u => u.toJSON())
-        }
-        response.status(404).end()
-    }).catch(error => next(error))
-})
 
 /*
 Saves a user to the database
