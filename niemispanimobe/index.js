1c658713bb2e060c35bca65df48035bdafd02ecc
TapioSaarnio <tapio.saarnio@gmail.com>
2021-05-05 20:21:06 +0300

    routing

--- niemispanimobe/index.js
+++ niemispanimobe/index.js
@@ -1,6 +1,7 @@
 const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
+const path = require('path')
 
 const server = http.createServer(app)
 app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../build', 'index.html')))
